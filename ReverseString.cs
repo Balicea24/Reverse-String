2ab4a3592666f33cf5f69af9329ad8cf81fe606a
Balicea24 <bralicea@syr.edu>
2018-12-17 22:22:51 -0500

    first commit

---
+++ ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace str {
+   class stringReverse {
+       static void Main(string[] args){
+         string a;
+         string newStr = "";
+         int x;
+
+         Console.Write("Enter a string: ");
+         a = Console.ReadLine();
+         
+         for (x = 1; x <= a.Length; x++){
+            newStr += a[a.Length - x];
+         }
+         Console.WriteLine(newStr); 
+      }
+   }
+} 
